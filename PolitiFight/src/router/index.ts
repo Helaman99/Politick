058d18144b610e3f2e902ad8fea4e346b6a2d467
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-12 15:08:10 -0700

    Sign Up page and route

--- PolitiFight/src/router/index.ts
+++ PolitiFight/src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/SignUpView.vue')
+    },
+    {
       path: '/home',
       name: 'home',
       component: HomeView
---
+++ PolitiFight/src/views/SignUpView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class = "signup">
+
+        <img src = "../assets/mainLogo.svg">
+        <br><br><br>
+
+        <label for = "email">Email</label>
+        <v-text-field type = "email" id = "email" variant = "solo" placeholder = "Email" />
+        <label for = "password">Password</label>
+        <v-text-field type = "password" id = "password" variant = "solo" placeholder = "Password" />
+        <label for = "password2">Repeat Password</label>
+        <v-text-field type = "password" id = "password2" variant = "solo" placeholder = "Repeat Password" />
+        <v-btn>Sign Up</v-btn>
+
+        <br><br>
+        <p>Already have an account?</p>
+        <router-link to = "/">Login</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+img {
+  width: 100%;
+}
+
+.signup {
+    width: 60%;
+}
+
+.v-input {
+    width: 60%;
+}
+
+@media (max-width: 1024px) {
+
+    .login {
+        width: 90%;
+    }
+    .v-input {
+        width: 65%;
+    }
+}
+
+</style>
