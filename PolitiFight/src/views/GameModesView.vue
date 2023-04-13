8a77fce7594a273a046ceb624c6ba46977e2e1d6
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-13 13:52:46 -0700

    Added GameModes and Topics views

---
+++ PolitiFight/src/views/GameModesView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class = 'gameModes'>
+        <div class = 'buttons'>
+            <v-btn>Battle Royal</v-btn>
+            <v-btn>Debate</v-btn>
+            <v-btn>Jury</v-btn>
+        </div>
+        <router-link to = 'topics'>Back to Topics</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+.buttons {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 4rem;
+}
+
+.v-btn {
+    font-size: larger;
+    width: 25%;
+    height: 8rem;
+}
+
+</style>
