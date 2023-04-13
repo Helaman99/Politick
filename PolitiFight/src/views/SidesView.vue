048ef86f37465b3fea16511cdeb84c479cbb7ac7
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-13 15:45:33 -0700

    `sides` and `roomSize` views added

---
+++ PolitiFight/src/views/SidesView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class = 'sides'>
+        <div class = 'sidesButtons'>
+            <v-btn to = 'roomSize'>Authoritarian</v-btn>
+            <v-btn to = 'roomSize'>Left</v-btn>
+            <v-btn to = 'roomSize'>Libertarian</v-btn>
+            <v-btn to = 'roomSize'>Right</v-btn>
+        </div>
+        <router-link to = 'gameModes'>Back</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+.sidesButtons {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 2rem;
+    margin-bottom: 2rem;
+}
+.sidesButtons .v-btn {
+    font-size: larger;
+    height: 8rem;
+}
+
+@media (max-width: 870px) {
+    .sidesButtons {
+        grid-template-columns: 1fr 1fr;
+    }
+}
+
+@media (max-width: 500px) {
+    .sidesButtons {
+        grid-template-columns: 1fr;
+    }
+}
+
+</style>
