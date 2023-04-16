926075c4bc7897d4fd7dd79d2ddb669dfceed000
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-15 19:23:58 -0700

    PlayerCard component added

---
+++ PolitiFight/src/components/PlayerCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card class = 'playerCard' elevation = '5' :color = color>
+        <img :src = avatar alt = 'avatar' />
+        <v-card-title>{{ title }}</v-card-title>
+    </v-card>
+</template>
+
+<style scoped>
+
+img {
+    width: 75%;
+    border-radius: 2%;
+    box-shadow: 2px 2px 10px black;
+}
+
+.playerCard {
+    width: 50%;
+    padding: 1rem 0;
+}
+
+</style>
+<script>
+export default {
+    props: ['avatar', 'title', 'color']
+}
+</script>
