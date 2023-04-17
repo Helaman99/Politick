53b7f5ce0c9958fe5e9e74af79477ba4a9314809
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-17 16:29:29 -0700

    Create retrievePlayer.ts
    File for obtaining the user's data from the backend, and then initializing the players data within the object for future reference.

---
+++ PolitiFight/src/scripts/retrievePlayer.ts
@@ -0,0 +1,17 @@
+import Player from './Player'
+export default {
+    // Code to retrieve player data from backend/database
+
+    player: new Player (
+        '',
+        '',
+        0,
+        0,
+        0,
+        [0, 0, 0],
+        [0, 0, 0, 0],
+        [],
+        [],
+        [],
+    )
+}
