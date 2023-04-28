2669f8baf08325365724ea989a4e50f13b62f4f5
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-27 23:53:12 -0700

    `Player` object and backend API

---
+++ Politio.Api/Politio.Api/Controllers/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Politio.Api.Data;
+using Politio.Api.Services;
+
+namespace Politio.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlayerController
+{
+    private readonly PlayerService _playerService;
+
+    public PlayerController(PlayerService playerService)
+    {
+        _playerService = playerService;
+    }
+
+    [HttpGet]
+    public bool Login(string email, string password)
+    {
+        return _playerService.Login(email, password);
+    }
+
+    [HttpGet]
+    public string GetPlayerData(string email)
+    {
+        return _playerService.GetPlayerData(email);
+    }
+
+    [HttpPost]
+    public void UpdateCard(string email, string avatar, string title)
+    {
+        _playerService.UpdateCard(email, avatar, title);
+    }
+
+    [HttpPost]
+    public void AddCoins(string email, int amount)
+    {
+        _playerService.AddCoins(email, amount);
+    }
+
+    [HttpPost]
+    public void RemoveCoins(string email, int amount)
+    {
+        _playerService.RemoveCoins(email, amount);
+    }
+
+    [HttpPost]
+    public void UpdatePassword(string email, string password)
+    {
+        _playerService.UpdatePassword(email, password);
+    }
+}
---
+++ Politio.Api/Politio.Api/Services/PlayerService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Politio.Api.Data;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Politio.Api.Services;
+
+public class PlayerService
+{
+    private readonly AppDbContext _db;
+    public PlayerService(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Player> GetPlayer(string email)
+    {
+        List<Player> result = await _db.Players.Where(player => player.Email == email).ToListAsync();
+        if (!result.IsNullOrEmpty())
+            return result.First();
+        return null;
+    }
+
+    public string GetPlayerData(string email)
+    {
+        Player player = GetPlayer(email).Result;
+        if (player is not null)
+            return player.GetData();
+        return "Null";
+    }
+
+    public bool Login(string email, string password)
+    {
+        Player player = GetPlayer(email).Result;
+        if (player is not null)
+            return player.CheckPassword(password);
+        return false;
+    }
+
+    public void UpdateCard(string email, string avatar, string title)
+    {
+        Player player = GetPlayer(email).Result;
+        if (player is not null)
+        {
+            player.Avatar = avatar;
+            player.Title = title;
+        }
+    }
+
+    public void AddCoins(string email, int amount)
+    {
+        Player player = GetPlayer(email).Result;
+        if (player is not null)
+        {
+            player.CoinsTotal += amount;
+        }
+    }
+
+    public void RemoveCoins(string email, int amount)
+    {
+        Player player = GetPlayer(email).Result;
+        if (player is not null)
+        {
+            player.CoinsTotal -= amount;
+        }
+    }
+
+    public void UpdatePassword(string email, string password)
+    {
+        Player player = GetPlayer(email).Result;
+        if (player is not null)
+            player.Password = password;
+    }
+}
