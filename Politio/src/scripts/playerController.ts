96b07af93c7356ee997ed4a085833bbb9c41534a
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-29 13:02:27 -0700

    Update playerController.ts

--- Politio/src/scripts/playerController.ts
+++ Politio/src/scripts/playerController.ts
@@ -1,28 +1,46 @@
-import { Player } from './Player'
+import type { Player } from './Player'
 import Axios from 'axios'
 
 // Code to retrieve player data from backend/database
 
 // Create a player object with the player's data from the database
-let player: Player = new Player (
-    1,
-    'Happy Banana',
-    'astronaut.jpg',
-    0,
-    0,
-    0,
-    0,
-    [0, 0, 0],
-    [0, 0, 0, 0],
-    ['Happy', 'Sad', 'Wrinkly', 'Bloated'],
-    ['Helicopter', 'Banana', 'Sasquatch', 'Bunny', 'Marshmallow', 'Tank', 'Goldfish'],
-    ['astronaut.jpg', 'astronaut-synthwave.jpg'],
-    0,
-    'light'
-)
+let player: Player;
+
+function initializePlayer(id: number) {
+    /*
+    playerData = axios call to backend GetPlayerData method
+    player = new Player (
+        1,
+        'Happy Banana',
+        'astronaut.jpg',
+        0,
+        0,
+        0,
+        0,
+        [0, 0, 0],
+        [0, 0, 0, 0],
+        ['Happy', 'Sad', 'Wrinkly', 'Bloated'],
+        ['Helicopter', 'Banana', 'Sasquatch', 'Bunny', 'Marshmallow', 'Tank', 'Goldfish'],
+        ['astronaut.jpg', 'astronaut-synthwave.jpg'],
+        0,
+        'light'
+    )
+    */
+}
+
 const pathToAvatars = '/src/assets/avatars/'
 
 export { player, pathToAvatars }
+
+export function login(email: string, password: string) {
+    /* 
+    result = axios call to backend login method
+    if (result != -1)
+        initializePlayer(result);
+    return result;
+    */
+}
+
 export function updateCard(newAvatar: string, newTitle: string) {
     console.log("Updating player data in the database...")
     player.avatar = newAvatar
