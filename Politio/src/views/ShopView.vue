7e5c5a74260b3ab18f22d27073061c3838b54414
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-30 21:53:07 -0700

    Create ShopView.vue

---
+++ Politio/src/views/ShopView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class = 'shop'>
+        <h1>Shop</h1>
+        <br>
+        <Transition name = 'fade'>
+            <RouterView />
+        </Transition>
+    </div>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
