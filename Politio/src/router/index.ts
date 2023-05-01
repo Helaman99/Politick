192cd9b67b431ef7ae2c382670bb09dc0c71de70
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-04-30 21:53:24 -0700

    Paths and routes

--- Politio/src/router/index.ts
+++ Politio/src/router/index.ts
@@ -26,7 +26,8 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },{
+    },
+    {
       path: '/chat',
       name: 'chat',
       component: () => import('../views/ChatView.vue')
@@ -70,6 +71,33 @@ const router = createRouter({
           path: 'sides',
           name: 'sides',
           component: () => import('../views/SidesView.vue')
+        },
+        {
+          path: 'shop',
+          name: 'shop',
+          component: () => import('../views/ShopView.vue'),
+          children: [
+            {
+              path: '',
+              name: 'choices',
+              component: () => import('../views/ShopChoicesView.vue')
+            },
+            {
+              path: 'avatars',
+              name: 'avatars',
+              component: () => import('../views/ShopAvatarsView.vue')
+            },
+            {
+              path: 'titles',
+              name: 'titles',
+              component: () => import('../views/ShopTitlesView.vue')
+            },
+            {
+              path: 'mystery-boxes',
+              name: 'mystery-boxes',
+              component: () => import('../views/MysteryBoxesView.vue')
+            }
+          ]
         }
       ]
     }
