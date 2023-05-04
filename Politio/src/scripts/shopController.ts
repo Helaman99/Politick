a183b6ddd6c14d40bc694f294ff84e0ea4984cf3
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-04 16:05:09 -0700

    `GetAvatars` is 100% functional

--- Politio/src/scripts/shopController.ts
+++ Politio/src/scripts/shopController.ts
@@ -1,12 +1,1 @@
 import Axios from 'axios'
-
-let avatarsList = Array<string>
-Axios.get('https://localhost:7060/Player/GetAvatars')
-                    .then((response) => {
-                        avatarsList(response.data)
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                    })
-
-export default { avatarsList }
--- Politio/src/views/ShopAvatarsView.vue
+++ Politio/src/views/ShopAvatarsView.vue
@@ -3,12 +3,59 @@
         <h2>Avatars</h2>
         <router-link to = '/dashboard/shop/'>Back</router-link>
 
-        <div class = 'avatars'>
-            <p v-for = 'name in shopController.avatarsList'>{{ name }}</p>
+        <div class = 'avatarOptions'>
+            <div class = "avatarImage" v-for = 'name in avatarList'>
+                <img :src = pathToAvatars.concat(name)>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang = 'ts'>
-import shopController from '@/scripts/shopController'
+//import shop from '@/scripts/shopController'
+import Axios from 'axios'
+import { ref } from 'vue'
+import { pathToAvatars } from '@/scripts/playerController'
+
+const avatarList = ref<string[]>([])
+
+Axios.get('https://localhost:7060/Shop/GetAvatars')
+                    .then((response) => {
+                        avatarList.value = response.data
+                        console.log(response.data)
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
 </script>
+
+<style scoped>
+.avatarOptions {
+    width: 50%;
+    height: 50%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    justify-items: center;
+    align-items: center;
+}
+.avatarOptions .avatarImage {
+    width: 80%;
+    padding: 3%;
+    transition-duration: 0.25s;
+}
+.avatarOptions .avatarImage:hover {
+    padding: 0;
+}
+.avatarOptions .avatarImage:active {
+    padding: 3%;
+}
+.avatarOptions .avatarImage img {
+    width: 100%;
+    border-radius: 5%;
+    display: block;
+    transition-duration: 0.25s;
+}
+.avatarOptions .avatarImage img:hover {
+    box-shadow: 1px 1px 5px black;
+}
+</style>
