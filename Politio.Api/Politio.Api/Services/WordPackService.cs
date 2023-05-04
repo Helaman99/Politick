1e317fce904991ffe195e1a0b671539a76d1c6e6
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-03 22:00:14 -0700

    Update WordPackService.cs
    Got rid of the default constructor

--- Politio.Api/Politio.Api/Services/WordPackService.cs
+++ Politio.Api/Politio.Api/Services/WordPackService.cs
@@ -3,7 +3,5 @@ namespace Politio.Api.Services;
 
 public class WordPackService
 {
-    public WordPackService() { }
-
     public WordPack BasicList = new("Basic Words", new string[] {"word1", "word2"});
 }
