a3a26813054bfa645cb262b5a163a504dcb2b207
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-03 22:03:38 -0700

    Create MysteryBoxService.cs
    This service will instantiate and keep track of the different available mystery boxes;

---
+++ Politio.Api/Politio.Api/Services/MysteryBoxService.cs
@@ -0,0 +1,7 @@
+﻿using Politio.Api.Data;
+namespace Politio.Api.Services;
+
+public class MysteryBoxService
+{
+    public MysteryBox ThemedAvatarBox = new("Themed Avatars", new[] { "path1", "path2" });
+}
