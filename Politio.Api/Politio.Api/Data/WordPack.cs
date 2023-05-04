5aab8375552d1f62078889610f0627d4cd035c7d
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-03 21:50:57 -0700

    Create WordPack.cs
    The `WordPack` object will be used to represent purchasable word 'packs'

---
+++ Politio.Api/Politio.Api/Data/WordPack.cs
@@ -0,0 +1,13 @@
+﻿namespace Politio.Api.Data;
+
+public class WordPack
+{
+    public string Name { get; set; }
+    public string[] Words { get; set; }
+
+    public WordPack(string name, string[] words)
+    {
+        Name = name;
+        Words = words;
+    }
+}
