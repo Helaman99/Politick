71dba4505b2fbc54a07e1d1a595659d0cf792c5d
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-03 21:51:32 -0700

    Create WordPackService.cs
    This service will instantiate and hold all the purchasable `WordPack`s

---
+++ Politio.Api/Politio.Api/Services/WordPackService.cs
@@ -0,0 +1,9 @@
+﻿using Politio.Api.Data;
+namespace Politio.Api.Services;
+
+public class WordPackService
+{
+    public WordPackService() { }
+
+    public WordPack BasicList = new("Basic Words", new string[] {"word1", "word2"});
+}
