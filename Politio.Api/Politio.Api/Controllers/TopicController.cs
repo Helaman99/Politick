f294377c73976c3c37fca88b9f25a90dd3a69905
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-09 12:24:06 -0700

    Restoring accidentally deleted files

---
+++ Politio.Api/Politio.Api/Controllers/TopicController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Politio.Api.Services;
+using Politio.Api.Data;
+
+namespace Politio.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TopicController
+{
+    [HttpGet("GetTopics")]
+    public Topic[] GetTopics()
+        => TopicService.Topics;
+}
