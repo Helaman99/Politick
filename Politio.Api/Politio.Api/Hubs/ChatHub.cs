c874500851accd15709ee62d3e3fede76175ee0b
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-09 12:11:50 -0700

    Getting chat working

---
+++ Politio.Api/Politio.Api/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace Politio.Api.Hubs;
+
+public class ChatHub : Hub
+{
+    public Task JoinGroup(string groupName)
+    {
+        return Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+    }
+
+    public Task SendMessageToGroup(string groupName, string message)
+    {
+        return Clients.Group(groupName).SendAsync("ReceiveMessage", message);
+    }
+}
--- Politio/src/views/ChatView.vue
+++ Politio/src/views/ChatView.vue
@@ -1,5 +1,58 @@
 <template>
     <div class = 'chat'>
         Chat view.
+        <div class = "messages"></div>
+        <input v-model = 'message' placeholder = "Message" />
+        <v-btn @click = "SendMessage()">Send</v-btn>
     </div>
 </template>
+
+<script setup lang = 'ts'>
+import * as signalR from '@microsoft/signalr';
+
+let group = "Group1"
+let message: string
+const messagesElement = document.getElementsByClassName("messages")[0]
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl('https://localhost:7060/ChatHub')
+    .build()
+
+connection.start().then(() => {
+    connection.invoke('JoinGroup', group)
+})
+
+connection.on('ReceiveMessage', (message: string) => {
+    console.log("Received message: " + message)
+
+    const messageCard = document.createElement('v-card')
+    messageCard.classList.add('received-message')
+
+    const cardText = document.createElement('v-card-text')
+    cardText.textContent = message
+    messageCard.appendChild(cardText)
+})
+
+function SendMessage() {
+    connection.invoke('SendMessageToGroup', group, message)
+    console.log("Sent message: " + message)
+
+    const messageCard = document.createElement('v-card')
+    messageCard.classList.add('sent-message')
+
+    const cardText = document.createElement('v-card-text')
+    cardText.textContent = message
+    messageCard.appendChild(cardText)
+}
+</script>
+
+<style scoped>
+.received-message {
+    background-color: gainsboro;
+    color: black;
+}
+.sent-message {
+    background-color: lightskyblue;
+    color: black;
+}
+</style>
