56325066a00bc6ab3729d46c0acbf9d5a6c72f9c
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-09 17:02:23 -0700

    Create ChatFooter.vue
    Started the footer component

---
+++ Politio/src/components/ChatFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card>
+        <v-layout>
+            <v-footer>
+                <v-textarea v-model = 'message' placeholder = "Message" auto-grow 
+                    autocomplete />
+                <v-btn @click = "$emit('send', message)">Send</v-btn>
+            </v-footer>
+        </v-layout>
+    </v-card>
+</template>
+
+<script setup lang = 'ts'>
+import { ref } from 'vue'
+const message = ref('')
+</script>
