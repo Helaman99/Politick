d5995906323f33932ad87ac11fb63a5b4280699d
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-09 17:02:14 -0700

    Create MessageBubble.vue
    Reusable component that will be the bubbles in the chat

---
+++ Politio/src/components/MessageBubble.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card :class = class elevation = '0'>
+        <v-card-title>{{ text }}</v-card-title>
+    </v-card>
+</template>
+
+<script lang = 'ts'>
+export default {
+    props: ["class", "text"]
+}
+</script>
+
+<style scoped>
+.v-card {
+    width: fit-content;
+    margin: 0.5rem;
+}
+.received-message {
+    background-color: gainsboro;
+    color: black;
+}
+.sent-message {
+    background-color: lightskyblue;
+    color: black;
+}
+</style>
