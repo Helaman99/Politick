c589e17fa363576701dc3a4d1d70d66f01e7845f
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-21 10:46:18 -0700

    Title shop functionality in progress

--- Politick/src/scripts/shopController.ts
+++ Politick/src/scripts/shopController.ts
@@ -2,15 +2,12 @@ import Axios from 'axios'
 import { ref } from 'vue'
 import { removeCoins, addAvatar, } from '@/scripts/playerController'
 
-let pathToMysteryBoxes = "/src/assets/mystery_boxes/"
-
-export { pathToMysteryBoxes }
+// ---------- Avatar Shop Stuff ---------- \\
 
 const basicAvatarFileNames = ref([''])
 const premiumAvatarFileNames = ref([''])
 Axios.get("https://localhost:7060/Shop/BasicAvatars")
     .then((response) => {
-        console.log(response)
         basicAvatarFileNames.value = response.data
     })
     .catch((error) => {
@@ -18,7 +15,6 @@ Axios.get("https://localhost:7060/Shop/BasicAvatars")
     })
 Axios.get("https://localhost:7060/Shop/PremiumAvatars")
     .then((response) => {
-        console.log(response)
         premiumAvatarFileNames.value = response.data
     })
     .catch((error) => {
--- Politick/src/views/ShopTitlesView.vue
+++ Politick/src/views/ShopTitlesView.vue
@@ -1,6 +1,64 @@
+<script setup lang = 'ts'>
+import { player } from '@/scripts/playerController'
+import { ref } from 'vue'
+import Axios from 'axios'
+                
+const firstWordPacks = ref()
+const secondWordPacks = ref()
+const selectedPack = ref()
+Axios.get("https://localhost:7060/Shop/FirstWordPacks")
+    .then((response) => {
+        firstWordPacks.value = response.data
+        selectedPack.value = response.data[0]
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+Axios.get("https://localhost:7060/Shop/SecondWordPacks")
+    .then((response) => {
+        secondWordPacks.value = response.data
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+</script>
+
 <template>
     <div class = 'titles'>
         <router-link to = '/dashboard/shop/'>Back</router-link>
         Titles
+
+        <div class = 'main-content'>
+
+            <div class = 'left-side'>
+                <v-btn v-for = 'wordPack in firstWordPacks'
+                    @click = 'console.log(wordPack);selectedPack.value = wordPack'
+                    :disabled = '(player.unlockedTitleFirstWords.includes(wordPack.contents[0])
+                                || player.unlockedTitleSecondWords.includes(wordPack.contents[0]))'>
+                    {{ wordPack.name }}
+                </v-btn>
+            </div>
+
+            <div class = 'right-side' v-if = 'selectedPack'>
+                <p v-for = 'word in selectedPack.contents'>
+                    {{ word }}
+                </p>
+            </div>
+
+        </div>
     </div>
 </template>
+
+
+<style scoped>
+.main-content{
+    display: flex;
+    place-content: center;
+}
+.left-side {
+    width: 50%;
+}
+.right-side {
+    width: 50%;
+}
+</style>
