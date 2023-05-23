24c0c5e2144626d96218bf58ff3855eb3bc0423d
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-23 13:14:12 -0700

    Adding word packs

--- Politick.Api/Politick.Api/Services/BoxService.cs
+++ Politick.Api/Politick.Api/Services/BoxService.cs
@@ -17,13 +17,23 @@ public static class BoxService
 
     public static readonly List<Box> FirstWordPacks = new()
     {
-        new("Military Ranks", new[] { "Colonel", "Captain", "Commander", "General" }, 4),
-        new("Gloomy", new[] { "Gloomy", "Unhappy", "Depressed", "Rainy", "Overcast" }, 5),
+        new("Gloomy", new[] { "Gloomy", "Unhappy", "Depressed", "Rainy", "Overcast", "Pessimistic" }, 6),
+        new("Happy", new[] { "Sunny", "Cheerful", "Hopeful", "Optimistic", "Bubbly" }, 5),
+        new("Military Ranks", new[] { "Corporal", "Sergeant", "Lieutenant", "Captain", "Major", "Colonel", "Commander", "General" }, 8),
+        new("Textures", new[] { "Sticky", "Slimy", "Smooth", "Rough", "Bouncy", "Hard", "Soft" }, 7),
+        new("Flavors", new[] { "Cheesy", "Salty", "Sour", "Tart", "Sweet", "Savory", "Spicy", "Bitter" }, 8),
+        new("Extrovert", new[] { "Social", "Inquisitive", "Adventurous", "Extroverted" }, 4),
+        new("Introvert", new[] { "Anti-Social", "Uninterested", "Indifferent", "Unadventurous", "Introverted" }, 5),
+        new("Emotions", new[] { "Angry", "Fearful", "Nervous", "Curious", "Intrigued", "Determined", "Confused" }, 7),
     };
 
     public static readonly List<Box> SecondWordPacks = new()
     {
-        new("Fruit", new[] { "Orange", "Apple", "Grape", "Pineapple", "Strawberry" }, 5),
-        new("Military Equipment", new[] { "M1A1 Abrams", "Battleship", "Battlecruiser", "Missle", "Drone", "A-10 Warthog", "AC-130" }, 7),
+        new("Fruit", new[] { "Orange", "Apple", "Grape", "Pineapple", "Strawberry", "Watermelon" }, 6),
+        new("Nature", new[] { "Tree", "Bush", "Leaf", "Plant", "Rock", "Boulder" }, 6),
+        new("Crawly Things", new[] { "Ant", "Bumblebee", "Ladybug", "Tarantula", "Stinkbug", "Butterfly", "Caterpillar" }, 7),
+        new("Military Equipment", new[] { "M1A1 Abrams", "Battleship", "Destroyer", "Missile", "Drone", "A-10 Warthog", "AC-130" }, 7),
+        new("Military Ranks", new[] { "Corporal", "Sergeant", "Lieutenant", "Captain", "Major", "Colonel", "Commander", "General" }, 8),
+        new("Transportation", new[] { "Bus", "Car", "Train", "Truck", "Semi", "Sportscar", "ATV" }, 7),
     };
 }
