c3f43e8fba29e87722ebdc77f54586e9a1a4beb2
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-23 18:05:15 -0700

    `Opponent` object will keep track of player data in games

---
+++ Politick.Api/Politick.Api/Data/Opponent.cs
@@ -0,0 +1,15 @@
+﻿namespace Politick.Api.Data;
+
+public class Opponent
+{
+    public int Id { get; set; }
+    public string Avatar { get; set; }
+    public string Title { get; set; }
+
+    public Opponent(int id, string avatar, string title)
+    {
+        Id = id;
+        Avatar = avatar;
+        Title = title;
+    }
+}
