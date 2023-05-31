98982a4660864b47cbf592d6ec3fde4cf6cfe23e
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-05-31 13:06:45 -0700

    Add `Email` to `Claims.cs` and remove `UserName`

--- Politick.Api/Politick.Api/Identity/Claims.cs
+++ Politick.Api/Politick.Api/Identity/Claims.cs
@@ -3,5 +3,5 @@ public class Claims
 {
     public const string UserId = "userId";
     public const string Random = "random";
-    public const string UserName = "userName";
+    public const string Email = "email";
 }
