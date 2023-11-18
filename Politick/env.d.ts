0f0c47250f0f156bca0af6d406af022dee1acbbc
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2023-11-18 13:29:20 -0800

    Update env.d.ts

--- Politick/env.d.ts
+++ Politick/env.d.ts
@@ -2,6 +2,7 @@
 declare const process: {
     env: {
       VUE_APP_BASE_URL: string;
+      VUE_APP_PAYPAL_CLIENT_ID: string;
       // add other environment variables here
     }
   } & typeof globalThis.process;
